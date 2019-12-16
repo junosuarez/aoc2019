4a253442581ba398b6e1971b6e2485d3c02dcac1
junosuarez <juno@suarez.email>
2019-12-15 16:46:10 -0800

    refactor with new terms

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,63 +1,63 @@
 fn main() {
-  let mut nums = parse("1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,1,13,23,27,1,6,27,31,2,31,13,35,1,9,35,39,2,39,13,43,1,43,10,47,1,47,13,51,2,13,51,55,1,55,9,59,1,59,5,63,1,6,63,67,1,13,67,71,2,71,10,75,1,6,75,79,1,79,10,83,1,5,83,87,2,10,87,91,1,6,91,95,1,9,95,99,1,99,9,103,2,103,10,107,1,5,107,111,1,9,111,115,2,13,115,119,1,119,10,123,1,123,10,127,2,127,10,131,1,5,131,135,1,10,135,139,1,139,2,143,1,6,143,0,99,2,14,0,0".to_string());
+  let mut memory = parse("1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,1,13,23,27,1,6,27,31,2,31,13,35,1,9,35,39,2,39,13,43,1,43,10,47,1,47,13,51,2,13,51,55,1,55,9,59,1,59,5,63,1,6,63,67,1,13,67,71,2,71,10,75,1,6,75,79,1,79,10,83,1,5,83,87,2,10,87,91,1,6,91,95,1,9,95,99,1,99,9,103,2,103,10,107,1,5,107,111,1,9,111,115,2,13,115,119,1,119,10,123,1,123,10,127,2,127,10,131,1,5,131,135,1,10,135,139,1,139,2,143,1,6,143,0,99,2,14,0,0".to_string());
   // before running the program, replace position 1 with the value 12 and replace position 2 with the value 2
-  nums[1] = 12;
-  nums[2] = 2;
-  nums = calculate(nums);
+  memory[1] = 12;
+  memory[2] = 2;
+  memory = calculate(memory);
 
-  println!("{}", render(nums));
+  println!("{}", render(memory));
 }
 
 fn parse(expr: String) -> Vec<i32> {
-  let nums: Vec<i32> = expr
+  let memory: Vec<i32> = expr
     .split(",")
     .map(|s| s.parse::<i32>().expect("x"))
     .collect();
-  return nums;
+  return memory;
 }
 
-fn render(nums: Vec<i32>) -> String {
-  return nums
+fn render(memory: Vec<i32>) -> String {
+  return memory
     .iter()
     .map(|s| s.to_string())
     .collect::<Vec<String>>()
     .join(",");
 }
 
-fn calculate(mut nums: Vec<i32>) -> Vec<i32> {
-  let mut cursor = 0;
+fn calculate(mut memory: Vec<i32>) -> Vec<i32> {
+  let mut instruction_pointer = 0;
 
   loop {
-    let opcode = nums[cursor];
+    let opcode = memory[instruction_pointer];
     if opcode == 99 {
       // halt immediately
       break;
     }
-    let left_op = nums[cursor + 1] as usize;
-    let right_op = nums[cursor + 2] as usize;
-    let dest = nums[cursor + 3] as usize;
+    let left_op = memory[instruction_pointer + 1] as usize;
+    let right_op = memory[instruction_pointer + 2] as usize;
+    let dest = memory[instruction_pointer + 3] as usize;
     println!("op {}", opcode);
     match opcode {
       1 => {
         // add
         println!("add {} {} to {}", left_op, right_op, dest);
-        nums[dest] = nums[left_op] + nums[right_op];
+        memory[dest] = memory[left_op] + memory[right_op];
+        instruction_pointer += 4;
       }
       2 => {
         // multiply
         println!("mul {} {} to {}", left_op, right_op, dest);
-        nums[dest] = nums[left_op] * nums[right_op];
+        memory[dest] = memory[left_op] * memory[right_op];
+        instruction_pointer += 4;
       }
       _ => {
         // error
-        panic!("error at cursor {}", cursor);
+        panic!("error at instruction_pointer {}", instruction_pointer);
       }
     }
-
-    cursor += 4;
   }
 
-  return nums;
+  return memory;
 }
 
 fn calc_str(expr: String) -> String {
@@ -65,44 +65,44 @@ fn calc_str(expr: String) -> String {
 }
 
 fn calculate_str(expr: String) -> String {
-  let mut nums: Vec<i32> = expr
+  let mut memory: Vec<i32> = expr
     .split(",")
     .map(|s| s.parse::<i32>().expect("x"))
     .collect();
 
-  let mut cursor = 0;
+  let mut instruction_pointer = 0;
 
   loop {
-    let opcode = nums[cursor];
+    let opcode = memory[instruction_pointer];
     if opcode == 99 {
       // halt immediately
       break;
     }
-    let left_op = nums[cursor + 1] as usize;
-    let right_op = nums[cursor + 2] as usize;
-    let dest = nums[cursor + 3] as usize;
+    let left_op = memory[instruction_pointer + 1] as usize;
+    let right_op = memory[instruction_pointer + 2] as usize;
+    let dest = memory[instruction_pointer + 3] as usize;
     println!("op {}", opcode);
     match opcode {
       1 => {
         // add
         println!("add {} {} to {}", left_op, right_op, dest);
-        nums[dest] = nums[left_op] + nums[right_op];
+        memory[dest] = memory[left_op] + memory[right_op];
       }
       2 => {
         // multiply
         println!("mul {} {} to {}", left_op, right_op, dest);
-        nums[dest] = nums[left_op] * nums[right_op];
+        memory[dest] = memory[left_op] * memory[right_op];
       }
       _ => {
         // error
-        panic!("error at cursor {}", cursor);
+        panic!("error at instruction_pointer {}", instruction_pointer);
       }
     }
 
-    cursor += 4;
+    instruction_pointer += 4;
   }
 
-  return nums
+  return memory
     .iter()
     .map(|s| s.to_string())
     .collect::<Vec<String>>()
